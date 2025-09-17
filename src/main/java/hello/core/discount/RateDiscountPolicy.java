38b3c3c9f260c27d19b70164597201817a96f069
JungHoon99 <greenjm1@nate.com>
2025-09-18 00:26:23 +0900

    [feat] Introduce AppConfig for dependency injection and implement RateDiscountPolicy

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            int rate = 10;
+            return price * rate / 100;
+        }
+        else{
+            return 0;
+        }
+    }
+}
