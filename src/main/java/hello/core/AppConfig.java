c342cd640b3c578ac1e35ecdbe308340b917a2e0
JungHoon99 <greenjm1@nate.com>
2025-09-20 23:47:50 +0900

    feat: Refactor AppConfig for Spring configuration and update application entry points

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /*
     * AppConfig를 사용으로 구성영역과 사용영역 분리
@@ -17,20 +19,23 @@ import hello.core.order.OrderServiceImpl;
     * AppConfig는 구체 클래스에 의존하지 않는다.
     * AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 설정
  */
+@Configuration
 public class AppConfig {
-
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     private static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     private static DiscountPolicy discountPolicy() {
 //        return new FIxDiscountPolicy();
         return new RateDiscountPolicy();
