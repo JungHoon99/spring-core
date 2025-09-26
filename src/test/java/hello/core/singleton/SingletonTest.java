d60c8d14bba25902eafc3e14b71b41f451890a9a
JungHoon99 <greenjm1@nate.com>
2025-09-26 23:00:24 +0900

    ### 새로운 싱글톤 관련 기능과 테스트 추가
    ---
    **제목 (Subject)**
    feat: 싱글톤 객체 생성 및 테스트 기능 추가
    
    **본문 (Body)**
    - 싱글톤 패턴을 사용하는 `SingletonService` 클래스 생성
    - 동일 객체인지 검증하는 `SingletonTest` 테스트 코드 작성
    - 객체 재사용 로직 및 동작 유효성 확인

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,19 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+}
