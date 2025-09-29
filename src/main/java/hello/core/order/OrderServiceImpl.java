ccb864610afca5e519702d0c2e96007e9a13e935
JungHoon99 <greenjm1@nate.com>
2025-09-29 22:27:38 +0900

    feat: 자동 설정을 위한 AutoAppConfig 클래스 및 테스트 추가 - 자동 구성 기능을 위한 AutoAppConfig 클래스 생성 - MemberService를 검증하는 AutoAppConfigTest 클래스 추가 - MemberServiceImpl, MemoryMemberRepository, OrderServiceImpl, RateDiscountPolicy에 @Component 어노테이션 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class OrderServiceImpl implements OrderService {
     // 엄밀히 따지면 인터페이스와 구현체 두개를 의존하고 있는 형태
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -14,6 +18,7 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
